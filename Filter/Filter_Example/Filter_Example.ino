2420c9a53b1541c504699aba542c8055e49a86c8
Nerd It Yourself <nerdityourself.firenze@gmail.com>
2017-10-17 16:35:00 +0200

    Add files via upload

---
+++ Filter/Filter_Example/Filter_Example.ino
@@ -0,0 +1,23 @@
+
+#include "filter.h"
+
+#define INPUT_PIN A0   
+
+Filter filtro;
+int sampleF;
+void setup() {
+
+Serial.begin(9600);
+filtro.setDepthFilter(10);     //imposta la lunghezza del filtro
+
+}
+
+void loop() {
+  
+  
+   sampleF = filtro.filterSamples(analogRead(INPUT_PIN)); 
+  
+   Serial.println(sampleF);
+   
+   delay(100);
+}
